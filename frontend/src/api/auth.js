ed269f27069391daa211b4c2b640ecf365aed7b9
Acla <acladominic10@gmail.com>
2025-04-21 23:21:31 +0800

    Authfix

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -5,6 +5,7 @@ export async function register(data) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Accept': 'application/json',
     },
     body: JSON.stringify(data),
   });
